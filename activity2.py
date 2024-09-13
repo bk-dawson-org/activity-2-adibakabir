9921ef8f0dbd085b18022b17b143116bb313088b
adibakabir <adiba.kabir@dawsoncollege.qc.ca>
2024-09-13 23:14:54 +0000

    tax.py

---
+++ activity2.py
@@ -0,0 +1,8 @@
+x = 4 * (5 / 2) ** 2
+print(x)
+
+celsius = 20
+print ("That\'s" , celsius , "degrees celsius !")
+
+y = 97 + 2 / 12 -18 * 3  %  2
+print (int(y))
---
+++ tax.py
@@ -0,0 +1,7 @@
+price = float(input("enter the price of the food"))
+pricetaxincluded = price * 0.18 + price
+print("this is the price tax included" , pricetaxincluded)
+
+
+x = "adiba " * 3
+print(x)
